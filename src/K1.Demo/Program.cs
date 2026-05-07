8d0080040e411d4c91d3a31bee9d7b100c9a89b6
Matt Tantony <matt.tantony@gmail.com>
2026-05-07 08:44:17 +0100

    feat: add demo console app

---
+++ src/K1.Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using K1.Modelling;
+using K1.Problems.Elements;
+using K1.Problems.NQueens;
+using K1.Solving;
+
+NQueensProblem problem = NQueensProblem.FromN(4);
+BinaryCsp<int, Square> binaryCsp = problem.AsBinaryCsp();
+SearchAlgorithm algorithm = new(TraversalStrategy.NaiveBacktracking, OrderingStrategy.NaturalOrder);
+BinaryCspSolver<int, Square> solver = BinaryCspSolver<int, Square>.WithStepDelay(TimeSpan.Zero);
+
+SolvingResult<int, Square> result = await solver.SolveAsync(binaryCsp, algorithm, CancellationToken.None);
+ValidationResult validationResult = problem.ValidSolution(result.Assignments);
+Console.WriteLine(validationResult);
